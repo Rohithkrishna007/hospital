ba7ac1a9bf7bf58b43c69a28a83bed19126c722d
Rohithkrishna007 <129927958+Rohithkrishna007@users.noreply.github.com>
2023-05-24 10:23:32 +0530

    Delete spring directory

--- spring/src/main/java/balaji/spring/repositories/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package balaji.spring.repositories;
-
-import balaji.spring.entities.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PatientRepository extends JpaRepository<Patient,Integer> {
-}
