a8673b026e86f460cad658cd355ca25833fd1b4b
Rohithkrishna007 <129927958+Rohithkrishna007@users.noreply.github.com>
2023-05-24 09:42:29 +0530

    Delete spring directory

--- spring/src/main/java/balaji/spring/repositories/DeanRepository.java
+++
@@ -1,9 +0,0 @@
-package balaji.spring.repositories;
-
-import balaji.spring.entities.Dean;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeanRepository extends JpaRepository<Dean,Integer>{
-}
